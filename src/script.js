485abaeefe84ccc86491d831245adab45ea63c23
Luiz Barros <lck.barros@gmail.com>
2025-01-12 16:01:36 -0300

    Adicionado foco no input, opção de pressionar o enter para enviar, e organização do header

--- src/script.js
+++ src/script.js
@@ -2,34 +2,39 @@ const tarefaInput = document.getElementById('tarefaInput');
 const adicionarBtn = document.getElementById('adicionarBtn');
 const lista = document.getElementById('lista');
 
-adicionarBtn.addEventListener('click', () => {
-  const tarefa = tarefaInput.value.trim();
-
-  if (tarefa === '') {
-    alert('Por favor, insira uma tarefa.');
-    return;
+tarefaInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+  adicionarTarefa();
   }
-  
+ });
+ 
+ adicionarBtn.addEventListener('click', adicionarTarefa);
+ 
+ function adicionarTarefa() {
+  const tarefa = tarefaInput.value.trim();
+  if (tarefa === '') {
+  alert('Por favor, insira uma tarefa.');
+  return;
+  }
+ 
   const itemTarefa = document.createElement('li');
   itemTarefa.textContent = tarefa;
- 
   const removerBtn = document.createElement('button');
   removerBtn.textContent = 'Remover';
   removerBtn.className = 'btn-remover';
  
   itemTarefa.addEventListener('click', () => {
-    itemTarefa.classList.toggle('completed');
+  itemTarefa.classList.toggle('completed');
   });
-
+ 
   removerBtn.addEventListener('click', () => {
-    lista.removeChild(itemTarefa);
+  lista.removeChild(itemTarefa);
   });
-
+ 
   itemTarefa.appendChild(removerBtn);
-   lista.appendChild(itemTarefa);
-  
-   tarefaInput.value = '';
- });
+  lista.appendChild(itemTarefa);
+  tarefaInput.value = '';
+ }
 
  window.onload = function() {
   document.getElementById("ano").innerHTML = new Date().getFullYear();
