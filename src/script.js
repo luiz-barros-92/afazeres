da186334d797b8cbb507c0ff20288c17c7976d2c
Luiz Barros <lck.barros@gmail.com>
2025-01-12 15:28:28 -0300

    Estilo dark, organização de outputs e implementação do rodapé com ano automático

--- src/script.js
+++ src/script.js
@@ -30,3 +30,7 @@ adicionarBtn.addEventListener('click', () => {
   
    tarefaInput.value = '';
  });
+
+ window.onload = function() {
+  document.getElementById("ano").innerHTML = new Date().getFullYear();
+ }
